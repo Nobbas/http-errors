ea3b28f11575018e8e52b48396e7f046a6870a67
Thibault Théologien <thibault@nobbas.com>
2019-07-04 12:26:28 -0400

    NOB-1941 Update exported errors + fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import 'module-alias/register';
 
+// Enums
+export * from './enums/http-error-type';
+
+// Models
 export * from './enums/http-status-code';
 export * from './models/error-response-object';
+
+// HTTP errors
+export * from './errors/http-error';
 export * from './errors/http-auto-display-error';
 export * from './errors/http-regular-error';
 export * from './errors/http-resource-not-found-error';
 export * from './errors/http-unauthenticated-request-error';
 export * from './errors/http-unauthorized-access-error';
+
+// Intern errors
 export * from './errors/translation-not-found-error';
+export * from './errors/invalid-translation-key-error';
